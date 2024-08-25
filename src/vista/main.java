b0154bd1e713b4474668c0c759ae9697f9bbb06d
Yeymi <ymendozag3@miumg.edu.gt>
2024-08-24 22:23:50 -0600

    primer commit

---
+++ src/vista/main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author yeymi
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        frm_cadenas frm = new frm_cadenas();
+        frm.show();
+        
+                
+    }
+    
+}
